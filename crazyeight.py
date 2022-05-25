450c5dd8b44744b41c90161f8f4e9ee8e28cee0d
NathanFran6 <nathandfran06@gmail.com>
2022-05-25 11:30:54 -0400

    testing still

---
+++ crazyeight.py
@@ -0,0 +1,12 @@
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit= suit
+
+
+cards={
+    'spades':[2,3,4,5,6,7,8,9,10, 'Jack', 'Queen', 'King']
+    'clubs':[2,3,4,5,6,7,8,9,10, 'Jack', 'Queen', 'King']
+    'hearts':[2,3,4,5,6,7,8,9,10, 'Jack', 'Queen', 'King']
+    'diamonds':[2,3,4,5,6,7,8,9,10, 'Jack', 'Queen', 'King']
+    }
